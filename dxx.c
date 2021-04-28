ad9ec2c5df7371d42e93b37dda9f7138ba2e05b0
harrystudypython <shenwenlong66@outlook.com>
2021-04-28 11:34:15 +0800

    seems not bug of chinese

--- dxx.c
+++ dxx.c
@@ -3,18 +3,17 @@
 
 int main(void)
 {
-    char* s[100] = {0};//s是一个字符指针数组，包含100个char类型的指针。同时还有指针数组的初始化。
-    char* p = s;//p是指向指针数组的指针
-    char input = {0};//定义一个字符变量，并初始化
+    char* s[100] = {0}。
+    char* p = s;//p is point,which point to s(point struct)
+    char input = {0};
     int i = 0;
     int j = 0;
     
-    gets(s);//这个是获取一行所有的字符，在遇到换行符后返回所有的字符。
-    scanf("%c", &input);//获取一个字符，注意scanf获取的停止标志是EOF///这里犯了错，字符应该是%c而不是%d，除了字符串都要&
-    
+    gets(s);//can not enter
+    scanf("%c", &input);//get char scanf end flag is EOF &  
     while (p[i] != '\0')
     {
-        if (tolower(p[i]) == tolower(input))//tolower是将字母转换为小写字母的函数
+        if (tolower(p[i]) == tolower(input))//tolower is make to little abc
         {
             j++;
         }
